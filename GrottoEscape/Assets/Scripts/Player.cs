41705d219cc8aa9b7472ff2f3a79db1a45e95caa
William Santos <williamdsw@outlook.com>
2019-12-28 22:35:15 -0300

    Grotto Escape -> Programando o Pulo

--- GrottoEscape/Assets/Scripts/Player.cs
+++ GrottoEscape/Assets/Scripts/Player.cs
@@ -4,16 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //-----------------------------------------------------------------//
+    // FIELDS
+
     // Config
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float jumpForce;
+    [SerializeField] private float maxSpeed = 1.5f;
+    [SerializeField] private float jumpForce = 150f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform bulletSpawn;
+    [SerializeField] private LayerMask layerGround;
 
     // State
-    private bool isGrounded;
-    private bool isJumping;
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private bool isJumping = false;
 
     // Cached
     private Rigidbody2D rigidBody2D;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+
+    //-----------------------------------------------------------------//
+    // MONOBEHAVIOUR FUNCTIONS
+
+    private void Awake () 
+    {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start () 
+    {
+        
+    }
+
+    private void Update () 
+    {
+        isGrounded = Physics2D.Linecast (this.transform.position, groundCheck.position, layerGround);
+
+        if (Input.GetButtonDown ("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate () 
+    {
+        if (isJumping)
+        {
+            rigidBody2D.AddForce (new Vector2 (0f, jumpForce));
+            isJumping = false;
+        }
+    }
 }
