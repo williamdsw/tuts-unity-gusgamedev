f76ab630a01bf9b138a47059cc9aebd072a67a17
William Santos <williamdsw@outlook.com>
2019-12-28 22:15:43 -0300

    Grotto Escape -> Criando o Player

---
+++ GrottoEscape/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Config
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpForce;
+
+    // State
+    private bool isGrounded;
+    private bool isJumping;
+
+    // Cached
+    private Rigidbody2D rigidBody2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+}
