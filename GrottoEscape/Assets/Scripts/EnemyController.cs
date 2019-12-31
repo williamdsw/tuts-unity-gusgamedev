5d348d5b4bb55cc8a241d73527d2385659ecf569
William Santos <williamdsw@outlook.com>
2019-12-31 12:21:49 -0300

    Grotto Escape -> Criando Controlador de Inimigos

---
+++ GrottoEscape/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //-----------------------------------------------------------------//
+    // FIELDS
+
+    // Config
+    [SerializeField] private int health;
+    [SerializeField] private int moveSpeed;
+
+    // State
+    protected bool isMoving = false;
+
+    // Cached
+    protected Rigidbody2D rigidBody2D;
+    protected Animator animator;
+    protected Transform player;
+
+    //-----------------------------------------------------------------//
+    // MONOBEHAVIOUR FUNCTIONS
+
+    private void Awake () 
+    {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+        player = GameObject.Find ("Player").GetComponent<Transform>();
+    }
+}
