515f6748000fae0cd831b18b99b80f230b9b4578
William Santos <williamdsw@outlook.com>
2021-02-08 15:44:21 -0300

    Criando um Top-Down Shooter com Unity - Parte 6/8

---
+++ top-down-shooter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // || Inspector References
+
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private GameObject enemyPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemies", 0.5f, 1f);
+    }
+
+    private void SpawnEnemies()
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemyPrefab, spawnPoints[index].position, Quaternion.identity);
+    }
+}
