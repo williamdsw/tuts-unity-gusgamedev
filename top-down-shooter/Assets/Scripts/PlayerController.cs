8db58d2b49ff133e75c78c3e2566a634d755ae4a
William Santos <williamdsw@outlook.com>
2021-02-07 22:38:35 -0300

    Criando um Top-Down Shooter com Unity - Parte 2/8

---
+++ top-down-shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // || Inspector References
+
+    [SerializeField] private float moveSpeed;
+
+    // || State
+
+    private Vector2 moveInput;
+
+    // || Cached References
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        transform.Translate(moveInput * Time.deltaTime * moveSpeed);
+
+        animator.SetBool("IsMoving", moveInput != Vector2.zero);
+    }
+}
