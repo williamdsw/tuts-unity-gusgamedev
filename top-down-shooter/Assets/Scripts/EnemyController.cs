4bb4b7d8cf745837809e613fc820b0146490bace
William Santos <williamdsw@outlook.com>
2021-02-08 12:22:38 -0300

    Criando um Top-Down Shooter com Unity - Parte 5/8

---
+++ top-down-shooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // || Inspector References
+
+    [SerializeField] private float speed;
+
+    // || Cached References
+
+    private GameObject player;
+
+    private Animator animator;
+
+    // || State
+
+    private bool isAlive = true;
+
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (player && isAlive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (animator)
+        {
+            if (other.CompareTag("Bullet") && isAlive)
+            {
+                animator.SetTrigger("Death");
+                isAlive = false;
+                Destroy(gameObject, 0.5f);
+            }
+        }
+    }
+}
